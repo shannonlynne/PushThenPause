3f36e15fb3b0b92ae090d83217e1d145e6001303
Shannon Morrell <shannon.morrell2@gmail.com>
2025-05-04 10:20:34 -0500

    Finished the backend for now

--- PushThenPause.Services/Dtos/DashboardDto.cs
+++ PushThenPause.Services/Dtos/DashboardDto.cs
@@ -5,5 +5,5 @@ public class DashboardDto
     public string DisplayName { get; set; } = string.Empty;
     public int StreakCount { get; set; }
     public bool NemsModEnabled { get; set; }
-    public CycleDto CycleVM { get; set; } = new CycleDto();
+    public List<CycleDto> CycleDtos { get; set; } = new List<CycleDto>();
 }
--- PushThenPause.Services/Services/DashboardService.cs
+++ PushThenPause.Services/Services/DashboardService.cs
@@ -1,6 +1,8 @@
-﻿using PushThenPause.Business.Dtos;
+﻿using Microsoft.EntityFrameworkCore;
+using PushThenPause.Business.Dtos;
 using PushThenPause.Business.Interfaces;
 using PushThenPause.Data;
+using PushThenPause.Data.Models;
 
 namespace PushThenPause.Business.Services
 {
@@ -16,22 +18,46 @@ namespace PushThenPause.Business.Services
         {
             DashboardDto dashboardDto = new DashboardDto();
 
-            //TODO: get info
-            //await _context 
-            //get user, get cycles, get those names of the break and user activities
-            //display those along with streak count and nemo mode
+            User? user = await _context.Users.FindAsync(userId);
+            if (user is not null)
+                dashboardDto.DisplayName = user.DisplayName;
+
+            List<Cycle> cycles = new List<Cycle>();
+            cycles = await _context.Cycles
+                .Where(c => c.UserId == userId)
+                .ToListAsync();
+
+            foreach (Cycle cycle in cycles)
+            {
+                BreakActivity? breakActivity = _context.BreakActivities
+                    .FirstOrDefault(c => c.BreakActivityId == cycle.BreakActivityId);
+                UserTask? userTask = _context.UserTasks
+                    .FirstOrDefault(c => c.UserTaskId == cycle.UserTaskId);
+
+                if (breakActivity is not null && userTask is not null)
+                {
+                    dashboardDto.CycleDtos.Add(
+                        new CycleDto
+                        {
+                            BreakActivityDuration = breakActivity.DurationMinutes,
+                            BreakActivityTitle = breakActivity.Title,
+                            UserTaskDuration = userTask.DurationMinutes,
+                            UserTaskTitle = userTask.Title
+                        });
+                }
+            }
+
+            StreakTracker? streakCount = await _context.StreakTrackers
+                .FirstOrDefaultAsync(s => s.UserId == userId);
+            if (streakCount is not null)
+                dashboardDto.StreakCount = streakCount.StreakCount;
+
+            NemsModeSettings? nemsModeSettings = await _context.NemsModeSettings
+                .FirstOrDefaultAsync(n => n.UserId == userId);
+            if (nemsModeSettings is not null)
+                dashboardDto.NemsModEnabled = nemsModeSettings.IsEnabled;
 
             return dashboardDto;
         }
-
-        public async Task<List<CycleDto>> AddCycle()
-        {
-            List<CycleDto> cycleDtos = new List<CycleDto>();
-
-            //Add a cycle
-            return cycleDtos;
-        }
-
-        //
     }
 }
